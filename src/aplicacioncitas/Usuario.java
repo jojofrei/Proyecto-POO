160377d27ae32eb6ff5b0c232051b268b8f735ce
jojofrei <67808003+jojofrei@users.noreply.github.com>
2020-07-27 04:12:25 -0500

    usuario mejorado 2.1

--- src/aplicacioncitas/Usuario.java
+++ src/aplicacioncitas/Usuario.java
@@ -191,14 +191,13 @@ public class Usuario {
             }
         } 
     }
-    
-    
-   //ESTE METODO SE VA A ENCARGAR QUE CADA CORREO QUE SE INGRESE ESTE BIEN ESCRITO Y NO ESTE REPETIDO 
-  public boolean ValidarMail() {
+
+    //ESTE METODO SE VA A ENCARGAR QUE CADA CORREO QUE SE INGRESE ESTE BIEN ESCRITO Y NO ESTE REPETIDO 
+    public boolean ValidarMail() {
     // Patron para validar el email
-   boolean validacion=true;
-   Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-   while (validacion == true){
+    boolean validacion=true;
+    Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    while (validacion == true){
         Scanner S=new Scanner(System.in);
         
         System.out.println("Ingrese Email:");
@@ -220,13 +219,13 @@ public class Usuario {
         else{
             System.out.println("Ingrese Email de nuevo");
         }
-   }
+    }
     Scanner S=new Scanner(System.in);
     return validacion;
     }
   
-  //ESTE METODO ME VA A PERMITIR CLASIFICAR DE QUE GENERO ES CADA PERSONA
- public void validarGenero(){
+    //ESTE METODO ME VA A PERMITIR CLASIFICAR DE QUE GENERO ES CADA PERSONA
+    public void validarGenero(){
     boolean val= true;
     Scanner S=new Scanner(System.in);
     while(val ==  true){
@@ -248,43 +247,26 @@ public class Usuario {
         }
     }
     
- }
- // ESTE METODO ME VA A PERMITIR QUE SE CALCULE LA EDAD DE LAS PERSONAS
+}
+    // ESTE METODO ME VA A PERMITIR QUE SE CALCULE LA EDAD DE LAS PERSONAS
  
      public static int calcular(Calendar fechaNac) {
 
         Calendar fechaActual = Calendar.getInstance();
 
- 
-
         // Cálculo de las diferencias.
-
         int years = fechaActual.get(Calendar.YEAR) - fechaNac.get(Calendar.YEAR);
-
         int months = fechaActual.get(Calendar.MONTH) - fechaNac.get(Calendar.MONTH);
-
         int days = fechaActual.get(Calendar.DAY_OF_MONTH) - fechaNac.get(Calendar.DAY_OF_MONTH);
-
- 
-
         // Hay que comprobar si el día de su cumpleaños es posterior
-
         // a la fecha actual, para restar 1 a la diferencia de años,
-
         // pues aún no ha sido su cumpleaños.
 
- 
-
         if(months < 0 // Aún no es el mes de su cumpleaños
-
            || (months==0 && days < 0)) { // o es el mes pero no ha llegado el día.
-
             years--;
-
         }
-
         return years;
-
     }
     
 }
