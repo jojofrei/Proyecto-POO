207251b0aa6fe78eaa4eb217485e5eb9854a10cd
jojofrei <67808003+jojofrei@users.noreply.github.com>
2020-07-27 03:46:02 -0500

    main

--- src/aplicacioncitas/AplicacionCitas.java
+++ src/aplicacioncitas/AplicacionCitas.java
@@ -21,11 +21,15 @@ public class AplicacionCitas {
     public static void main(String[] args) {
         Pregunta.llenarDatos();
         Perfil p1 = generarPerfil();
-        Perfil p2 = generarPerfil();
         allperfil.add(p1);
-        allperfil.add(p2);
         llenarListas(p1);
+        Perfil p2 = generarPerfil();
+        allperfil.add(p2);
         llenarListas(p2);
+        //Perfil p3 = generarPerfil();
+        //allperfil.add(p3);
+        //llenarListas(p3);
+        
         for(int i=0; i<allperfil.size(); i++){
             Perfil pr1 = allperfil.get(i);
             for(int j=i+1; j<allperfil.size(); j++){
@@ -33,11 +37,17 @@ public class AplicacionCitas {
                 pr1.buscarPareja(pr2);  
             }
         }
+        System.out.println(" ");
+        System.out.println("... Perfiles con los que hizo match ...");
         for(Perfil p :p1.getPerfiles()){
             System.out.println(p.getUser().toString());
         }
+        
     }
 
+    /*Este método generarPerfil me permitira producir un perfil, inmediatamente 
+    se haya creado un usuario*/
+    
     public static Perfil generarPerfil(){
         Usuario user = new Usuario();
         user.crearUsuario();
@@ -45,18 +55,26 @@ public class AplicacionCitas {
         return p;
     }
     
+    /*Este método me llenará las listas interesesUser y respuestasUser con la 
+    informacion escogida por el usuario, para de acuerdo a esto usarlas para
+    hacer la comparacion en la realizacion del match*/
+    
     public static void llenarListas(Perfil p){
         Scanner respuesta = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("----- Intereses disponibles -----");
         for(String str: interes){
                System.out.println(str);
            }
            System.out.println("Elija sus intereses: ");
            for(int i=0; i<3; i++){
-               String interes = respuesta.nextLine();
+               String interes = respuesta.nextLine().toLowerCase();
                p.getInteresesUser().add(interes);
            }
 
-        for(Pregunta preg:Pregunta.preguntas){
+        System.out.println(" ");
+        System.out.println("----- Cuestionario de preguntas -----");
+           for(Pregunta preg:Pregunta.preguntas){
             System.out.println(preg.getPregunta());
 
             for(String r :preg.getRespuestas()){
